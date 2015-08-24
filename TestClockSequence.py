f0e497bc368d829eb22b50818227617ada802538
phummel <phummel@paypal.com>
2015-08-24 00:35:37 -0700

    Explored 2 other options for solving this puzzle to make things go quicker.

--- TestClockSequence.py
+++ TestClockSequence.py
@@ -2,12 +2,41 @@
 
 import unittest
 from clock_sequence_506 import sum_sequence_values
+from clock_sequence_506 import sum_sequence_values2
+from clock_sequence_506 import sum_sequence_values3
 
 class TestClockSequence(unittest.TestCase):
 
     def test_sum_sequence_values(self):
         self.assertEquals(36120, sum_sequence_values(11))
+        self.assertEquals(260517, sum_sequence_values(15))
+        self.assertEquals(1494838, sum_sequence_values(16))
+        self.assertEquals(3838050, sum_sequence_values(17))
+        self.assertEquals(55072872, sum_sequence_values(29))
+        self.assertEquals(32875304, sum_sequence_values(30))
+        self.assertEquals(57808267, sum_sequence_values(31))
+        self.assertEquals(107138899, sum_sequence_values(32))
         self.assertEquals(18232686, sum_sequence_values(1000))
+
+        self.assertEquals(36120, sum_sequence_values2(11))
+        self.assertEquals(260517, sum_sequence_values2(15))    
+        self.assertEquals(1494838, sum_sequence_values2(16))    
+        self.assertEquals(3838050, sum_sequence_values2(17)) 
+        self.assertEquals(55072872, sum_sequence_values2(29))
+        self.assertEquals(32875304, sum_sequence_values2(30))
+        self.assertEquals(57808267, sum_sequence_values2(31))
+        self.assertEquals(107138899, sum_sequence_values2(32))
+        self.assertEquals(18232686, sum_sequence_values2(1000))            
+
+        self.assertEquals(36120, sum_sequence_values3(11))
+        self.assertEquals(260517, sum_sequence_values3(15))    
+        self.assertEquals(1494838, sum_sequence_values3(16))    
+        self.assertEquals(3838050, sum_sequence_values3(17)) 
+        self.assertEquals(55072872, sum_sequence_values3(29))
+        self.assertEquals(32875304, sum_sequence_values3(30))
+        self.assertEquals(57808267, sum_sequence_values3(31))
+        self.assertEquals(107138899, sum_sequence_values3(32))
+        self.assertEquals(18232686, sum_sequence_values3(1000))
 
 if __name__ == '__main__':
     unittest.main()
--- clock_sequence_506.py
+++ clock_sequence_506.py
@@ -17,46 +17,51 @@
 #
 # Find S(1014) mod 123454321.
 #
+# References:
+# http://eulersolutions.fr.yuku.com/topic/358/problem-506#.VdoocrcVfsY
+# https://github.com/Meng-Gen/ProjectEuler/blob/master/506.py
+#       (100000000000000, '=>', 18934502L)
+# https://projecteuler.net/thread=506
+# 
 ###############################################################################
 
 import time
 
-# About 2 hours for the initial solution.
-# TODOs:
-# accept input
-# optimize memory usage -- http://stackoverflow.com/questions/19926089/python-equivalent-of-java-stringbuffer
 
 ###############################################################################
 # Outputs the sum of the terms from the clock sequence.
 ###############################################################################
 def main():
     start = time.clock()
-    total_sum = sum_sequence_values(pow(10, 14))
+    total_sum = sum_sequence_values3(pow(10, 14))
     end = time.clock()
     elapsed_time = end - start
     print "The program took " + str(elapsed_time) + " seconds to execute."
 
 ###############################################################################
+#
 # Returns and outputs the following sum, S(n) from the specified nth value.
 # Let vn be the n'th value from the clock sequence. 
 # Where S(n) is v1 + v2 + ... + vn.
 #
 # Input: nth term from sequence
 # returns:  S(n)
+#
+# This was basically my original brute force method.  Not very performant.
+#
 ###############################################################################
 def sum_sequence_values(n):
-    base_number_seq = [1,2,3,4,3,2]
+    base_number_seq = (1,2,3,4,3,2)
     total_sum = 0
-    total_seq = base_number_seq
     current_seq_value = 0
     current_seq_sum = 0
     position = 0
     num = 1
     while num < n + 1:
         while num != current_seq_sum:
-            if position >= len(total_seq):
+            if position >= 6:  # len(base_number_seq)
                 position = 0
-            digit = total_seq[position]
+            digit = base_number_seq[position]
             position += 1
             current_seq_sum += digit
             current_seq_value = current_seq_value * 10 + digit
@@ -64,8 +69,83 @@ def sum_sequence_values(n):
         current_seq_value = 0
         current_seq_sum = 0
         num += 1
-    print "S(" + str(num) + ") mod 123454321 = " + str(total_sum % 123454321)
+    print "S(" + str(n) + ") mod 123454321 = " + str(total_sum % 123454321)
     return total_sum % 123454321
+
+
+###############################################################################
+# 
+# Returns and outputs the following sum, S(n) from the specified nth value.
+# Let vn be the n'th value from the clock sequence. 
+# Where S(n) is v1 + v2 + ... + vn.
+#
+# Input: nth term from sequence
+# returns:  S(n)
+#
+# This was my second attempt to get something faster.
+# The key was in recognizing that the sequence repeats after every 15.
+#
+###############################################################################
+def sum_sequence_values2(n, supress=False):
+    # 1, 2, 3, 4, 32, 123, 43, 2123, 432, 1234, 32123, 43212, 34321, 23432, 123432, 1234321, 2343212, . . .
+    # Sequence repeats every 15.
+    suffix = (0, 1, 2, 3, 4, 32, 123, 43, 2123, 432, 1234, 32123, 43212, 34321, 23432, 123432 )
+    # The prefixes all sum to 15
+    prefix = (123432, 123432, 234321, 343212, 432123, 321234, 123432, 432123, 212343, 432123, 123432, 321234, 432123, 343212, 234321, 123432)
+
+    total_sum = 0
+    position = 0
+    num = 1
+    while num < n + 1:
+        seq_for_n = num % 15
+        if seq_for_n == 0:
+            seq_for_n = 15
+        number_of_15s = num / 15
+        current_seq_value = 0
+
+        if num > 15:
+                # replicate the prefix for the number of sets of 15 we have
+                current_seq_value = int(str(prefix[seq_for_n]) * number_of_15s)
+
+        # Add the suffix to the current_seq_value
+        if num % 15 > 0 or current_seq_value == 0: 
+            shift = 10**len(str(suffix[seq_for_n]))
+            current_seq_value = current_seq_value * shift + suffix[seq_for_n]
+
+        total_sum += current_seq_value
+        num += 1
+    if not supress:
+        print "S(" + str(n) + ") mod 123454321 = " + str(total_sum % 123454321)
+    return total_sum % 123454321
+
+
+###############################################################################
+# 
+# Returns and outputs the following sum, S(n) from the specified nth value.
+# Let vn be the n'th value from the clock sequence. 
+# Where S(n) is v1 + v2 + ... + vn.
+#
+# Input: nth term from sequence
+# returns:  S(n)
+#
+# This was my third attempt and didn't require much thinking as the author
+# described the algorithm.
+#
+# From https://projecteuler.net/thread=506;page=2:        
+# There is a repetition of vn%123454321 and found that it repeats every 15*55555 
+# S(15*55555)%123454321 = 48521737 
+# Find the integer value of n/(15*55555) which shall be x 
+# Find n%(15*55555) which shall be i 
+# Then compute ( 48521737*x + S(i) ) mod 123454321
+#
+###############################################################################
+def sum_sequence_values3(n):
+        x = n / (15*55555)
+        i = n % (15*55555)
+        sum_i = sum_sequence_values2(i, True)
+        total_sum = 48521737 * x + sum_i
+        print "S(" + str(n) + ") mod 123454321 = " + str(total_sum % 123454321)
+        return total_sum % 123454321
 
 
 ###############################################################################
